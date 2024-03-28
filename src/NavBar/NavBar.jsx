7931fdfd6f75ed31178224534f6e6f9111c2b5cf
Shadow-monarch23 <aakashkafle47@gmail.com>
2024-03-28 15:16:15 +0545

    First commit

---
+++ src/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = () => {
+    return (
+        <div>
+            {/* Your navigation bar content goes here */}
+        </div>
+    );
+};
+
+export default NavBar;
